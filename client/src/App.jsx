a7375f6a7612253263e387969a82f9b6099f8ab2
PrashamsaS <prashamsashrestha28@gmail.com>
2024-10-08 11:28:08 +0545

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <Home/>
+      
     </>
   )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+   
+     <div className="card-footer" style={{marginTop:'36.8%', marginBottom:"0%",backgroundColor:"#2C303A"}}>
+            <form className="form-inline my-1 my-lg-0">
+      <button type="button" className="btn btn my-2 my-lg-0 mx-lg-1.5 text-light" style={{position:'relative', fontSize:'12px',margin:'0px 4px 0px 0px', padding:'3px 7px', backgroundColor:'#444857'}}>
+                         Console
+                            
+                        </button>
+                        <button type="button" className="btn btn my-2 my-lg-0 mx-lg-1.5 text-light" style={{position:'relative', fontSize:'12px', margin:'10px 4px 0px 0px', padding:'3px 7px', backgroundColor:'#444857'}}>
+                            Assets
+                            
+                        </button>
+                      
+                       
+                        <button type="button" className="btn btn my-2 my-lg-0 mx-lg-1.5 text-light" style={{position:'relative', fontSize:'12px', /* fontFamily:'"Lato", "Lucida Grande", "Lucida Sans Unicode", Tahoma, Sans-Serif', */ margin:'0px 4px 0px 0px', padding:'3px 7px', backgroundColor:'#444857' }}>
+                        Shortcuts
+                            
+                        </button>
+
+
+</form>
+    
+        </div>
+    
   )
 }
 
