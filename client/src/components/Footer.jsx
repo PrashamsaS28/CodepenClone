ae8e96ff4f06369699f57bc391e3e71eef793dd7
PrashamsaS <prashamsashrestha28@gmail.com>
2024-10-08 11:45:28 +0545

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Footer = () => {
   return (
    
-     <div className="card-footer" style={{marginTop:'36.8%', marginBottom:"0%",backgroundColor:"#2C303A"}}>
+     <div className="card-footer" style={{marginTop:'38.1%',backgroundColor:"#2C303A", padding:'5px 10px'}}>
             <form className="form-inline my-1 my-lg-0">
-      <button type="button" className="btn btn my-2 my-lg-0 mx-lg-1.5 text-light" style={{position:'relative', fontSize:'12px',margin:'0px 4px 0px 0px', padding:'3px 7px', backgroundColor:'#444857'}}>
+      <button type="button" className="btn btn mx-1 text-light" style={{position:'relative', fontSize:'12px',margin:'0', marginTop:'0px', padding:'2px 5px', backgroundColor:'#444857'}}>
                          Console
                             
                         </button>
-                        <button type="button" className="btn btn my-2 my-lg-0 mx-lg-1.5 text-light" style={{position:'relative', fontSize:'12px', margin:'10px 4px 0px 0px', padding:'3px 7px', backgroundColor:'#444857'}}>
+                        <button type="button" className="btn btn mx-1 text-light" style={{position:'relative', fontSize:'12px', margin:'0', padding:'2px 5px', backgroundColor:'#444857'}}>
                             Assets
                             
                         </button>
                       
                        
-                        <button type="button" className="btn btn my-2 my-lg-0 mx-lg-1.5 text-light" style={{position:'relative', fontSize:'12px', /* fontFamily:'"Lato", "Lucida Grande", "Lucida Sans Unicode", Tahoma, Sans-Serif', */ margin:'0px 4px 0px 0px', padding:'3px 7px', backgroundColor:'#444857' }}>
+                        <button type="button" className="btn btn mx-1 text-light" style={{position:'relative', fontSize:'12px', /* fontFamily:'"Lato", "Lucida Grande", "Lucida Sans Unicode", Tahoma, Sans-Serif', */ margin:'0', padding:'2px 5px', backgroundColor:'#444857' }}>
                         Shortcuts
                             
                         </button>
